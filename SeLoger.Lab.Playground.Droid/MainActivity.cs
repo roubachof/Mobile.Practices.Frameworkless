8103a597d15df836cfbe9bdd06772eebb80962e8
Roubachof <jm.alfonsi@gmail.com>
2017-01-14 23:26:25 +0100

    Add project files.

---
+++ SeLoger.Lab.Playground.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace SeLoger.Lab.Playground.Droid
+{
+    [Activity(Label = "SeLoger.Lab.Playground.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
